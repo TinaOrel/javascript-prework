220a4d6ecd5071caf2e3cc96e7c1cc2a072968dd
Valentyna Orel <orelvalenti@gmail.com>
2020-11-05 22:47:53 +0100

    js add first function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+if(randomNumber == 1){
+	computerMove = 'kamień';
+}elese if (randomNumber == 2){
+	computerMove = 'papier'
+}else if (randomNumber == 3){
+	computerMove = 'nożyce'
+}
